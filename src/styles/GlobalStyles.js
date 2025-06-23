17bb1483c61ac5b3b3c3090dee489c9643714646
pradeepsimhaj <pradeepsimhaj@gmail.com>
2025-06-23 17:00:59 +0530

    a little change to display the content in the center

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
   * { margin: 0; padding: 0; box-sizing: border-box; }
   html, body, #root {
+  display: flex;
+    flex-direction: column;
     height: 100%;
     font-family: Arial, sans-serif;
     background: black;
